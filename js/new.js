a998db822bab7e699ba1710c0984d8cd9d860415
rakesh <rakeshkumar748844@gmail.com>
2024-07-09 20:34:54 +0530

    add

--- js/new.js
+++ js/new.js
@@ -6,6 +6,7 @@ sideBtnId.addEventListener('click',()=>{
     // offerId.style.display='block'
 
     if (offerId.style.display=='block') {
+        // sideBtnId.style.width='100%'
         offerId.style.display='none'
     }else{
         offerId.style.display='block'
